566c7f3d75ef18f41a3360b9b698eac218562680
gindrianjafy <georges-antonio.indrianjafy@intercloud.com>
2021-09-22 19:00:10 +0200

    lint

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,8 @@
     <router-link to="/sachaL">SachaL</router-link> |
     <router-link to="/julien">Julien</router-link>
     <router-link to="/nicolasdegarrigues">NicolasDG</router-link> |
-    <router-link to="/sachag">SachaG</router-link> |
     <router-link to="/lucasC">LucasC</router-link> |
     <router-link to="/ahmed">Ahmed</router-link>
-
   </div>
   <router-view />
 </template>
