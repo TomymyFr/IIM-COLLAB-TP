5d18d9a99c3ce989e3525b8d612494e3a4b0fbd4
Fucao77 <axel.zidane@edu.devinci.fr>
2021-09-22 17:03:30 +0200

    fix_axel

--- src/views/Axel.vue
+++ src/views/Axel.vue
@@ -31,93 +31,88 @@
       <div class="photo">
         <img src="../assets/Axel.png" alt="Moi" />
       </div>
-
     </div>
 
     <article>
-        <div class="button">
-      <a id="Button" href="https://sass-maquette.vercel.app/">Projet Dev</a> 
-    </div>
+      <div class="button">
+        <a id="Button" href="https://sass-maquette.vercel.app/">Projet Dev</a>
+      </div>
     </article>
-    
-
   </section>
 </template>
 
-<style scoped>* {
-    padding: 0;
-    margin: 0;
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
 }
 
 section {
-    background-image: url("../assets/nova.png");
-    width: 100%;
-    height: 55vw;
+  background-image: url("../assets/nova.png");
+  width: 100%;
+  height: 55vw;
 }
 
 h1 {
-    font-size: 5rem;
+  font-size: 5rem;
 }
 
 .block1 {
-    color: white;
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    margin-top: 6vw;
-    padding-top: 20vw;
+  color: white;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 6vw;
+  padding-top: 20vw;
 }
 
 .skills {
-    width: 30vw;
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: nowrap;
+  width: 30vw;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: nowrap;
 }
 
 .stack {
-    margin-top: 20px;
-    width: 5vw;
-    height: 5vw;
+  margin-top: 20px;
+  width: 5vw;
+  height: 5vw;
 }
 
 .photo img {
-    width: 25vw;
-    height: 25vw;
-    border: none;
-    -moz-border-radius: 85px;
-    -webkit-border-radius: 85px;
-    border-radius: 85px;
+  width: 25vw;
+  height: 25vw;
+  border: none;
+  -moz-border-radius: 85px;
+  -webkit-border-radius: 85px;
+  border-radius: 85px;
 }
 
-
 article {
-    width: 100%;
-    display: flex;
-    justify-content: center;
+  width: 100%;
+  display: flex;
+  justify-content: center;
 }
 
 .button {
-    width: 15vw;
-    height: 4vw;
-    background-color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 15vw;
+  height: 4vw;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .button:hover {
-    background-color: rgb(113, 211, 113);
+  background-color: rgb(113, 211, 113);
 }
 
-.button a{
-    font-family: 'Roboto Mono', monospace;
-    font-size: 2rem;
-    text-decoration: none;
-    color: black;
-    font-weight: 500;
-    
+.button a {
+  font-family: "Roboto Mono", monospace;
+  font-size: 2rem;
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
 }
-
 </style>
