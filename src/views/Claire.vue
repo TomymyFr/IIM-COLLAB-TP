64b64ab503fb2f0752ad4f42292110798c636d86
Claire <claire.brisbart@edu.devinci.fr>
2021-09-22 15:19:35 +0200

    :lipstick: Finished style

--- src/views/Claire.vue
+++ src/views/Claire.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="container">
+    <div class="color"></div>
     <main>
+        <!-- title -->
+        <div class="title">
+            <img
+            src="https://avatars.githubusercontent.com/u/70761355"
+            alt="photo de Claire"
+            />
+            <h2>
+                Claire Brisbart - 
+                <span class="title_highlight">A3 DW Alt2</span>
+            </h2>
+            <hr>
+            <p>💼 Assistante chef de projet digital chez TotalEnergies</p>
+            <p>🦖 Co-présidente de la 404 Devinci</p>
+        </div>
 
-      <!-- title -->
-      <div class="title">
-        <img
-          src="https://avatars.githubusercontent.com/u/70761355"
-          alt="photo de Claire"
-        />
-        <h2>
-            Claire Brisbart - 
-            <span class="title_highlight">A3 DW Alt2</span>
-        </h2>
-        <p>💼 Assistante chef de projet digital chez TotalEnergies</p>
-        <p>🦖 Co-présidente de la 404 Devinci</p>
-      </div>
+        <!-- projets -->
+        <div class="project">
+            <a href="https://forum-associatif-numerique.fr">Forum associatif numérique</a>
+            <a href="https://github.com/Pouniflu/fil_rouge">Projet Fil Rouge</a>
+            <a href="https://github.com/Pouniflu/TD-VueJS">To do list Vue.js</a>
+            <a href="http://www.baijy.com">Baijy.com</a>
+        </div>
 
-       <!-- projets -->
-      <div class="project">
-        <a href="https://forum-associatif-numerique.fr">Forum associatif numérique</a>
-        <a href="https://github.com/Pouniflu/fil_rouge">Projet Fil Rouge</a>
-        <a href="https://github.com/Pouniflu/TD-VueJS">To do list Vue.js</a>
-        <a href="http://www.baijy.com">Baijy.com</a>
-      </div>
-
-      <!-- button -->
-      <div class="button">
-        <a href="https://www.clr-brisbart.fr">My Website</a>
-        <a href="https://github.com/Pouniflu">Github</a>
-      </div>
-            
+        <!-- button -->
+        <div class="button">
+            <a href="https://www.clr-brisbart.fr">Visit my 💻</a>
+            <a href="https://github.com/Pouniflu">Github 😺</a>
+        </div>
     </main>
   </div>
 </template>
@@ -42,10 +42,20 @@ export default {
 </script>
 
 <style scoped>
+.color {
+    position: absolute;
+    width: calc(100% - 8px);
+    height: 60vh;
+    background-color: #42B983;
+    z-index: -1;
+    bottom: 0;
+}
+
 main {
     width: 30%;
     border-radius: 26px;
-    box-shadow: 6px 3px 10px rgba(143, 143, 143, 0.226);
+    box-shadow: 6px 3px 10px rgba(143, 143, 143, 0.377);
+    background-color: white;
     padding: 40px;
     margin: 0 auto;
 }
@@ -73,6 +83,10 @@ main {
     color: rgb(152, 155, 158);
 }
 
+.title hr {
+    margin: 5px 0 16px;
+}
+
 .title_highlight {
     color: #42B983;
 }
@@ -92,6 +106,7 @@ main {
     text-decoration: none;
     font-style: italic;
     color: #2C3E50;
+    font-size: 14px;
 }
 
 .project a:hover {
@@ -99,4 +114,30 @@ main {
     color: #0a1b13;
 }
 
+/* Buttons */
+.button {
+    margin: 40px 0 20px;
+}
+
+.button a {
+    border: solid #42B983 3px;
+    padding: 10px 18px;
+    border-radius: 18px;
+    margin: 10px 20px;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: bold;
+    color: #42B983;
+    font-size: 18px;
+}
+
+.button a:hover {
+    box-shadow: 6px 3px 10px #42b98375;
+}
+
+.button a:first-of-type {
+    border: solid #42B983 3px;
+    color: white;
+    background-color: #42B983;
+}
 </style>
